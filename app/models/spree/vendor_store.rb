1e38d657f441503245967867bf8d3e3d0c85bdc1
Samoolino <samo4au@gmail.com>
2026-09-20 16:35:13 +0100

    feat(geo): expose store classification and geofence state

--- app/models/spree/vendor_store.rb
+++ app/models/spree/vendor_store.rb
@@ -9,9 +9,25 @@ module Spree
     validates :name, presence: true, uniqueness: { scope: :vendor_id, case_sensitive: false }
     validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }, allow_nil: true
     validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }, allow_nil: true
+    validates :country_code, length: { is: 2 }, allow_nil: true
 
     def geo_configured?
       latitude.present? && longitude.present?
+    end
+
+    def geographic_classification
+      {
+        country_code: country_code,
+        admin_area_1_code: admin_area_1_code,
+        admin_area_2_code: admin_area_2_code,
+        locality: locality,
+        postal_code: postal_code,
+        timezone: timezone
+      }.compact
+    end
+
+    def geofence_configured?
+      geo_fence.present? && geo_fence != {}
     end
   end
 end
